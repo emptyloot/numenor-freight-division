77e3cb1df24a256c6aa916f512573ea130f891d7
emptyloot <daviddcmmoo@gmail.com>
2025-10-20 00:21:57 -0700

    Background blur add

---
+++ src/components/background/Background.js
@@ -0,0 +1,13 @@
+import backgroundmap from '../../assets/bitcraftmapbackground.png'
+
+function Background() {
+    return (
+        <div className='fixed top-0 left-0 w-full h-full z-[-1] bg-cover bg-center'
+            style={{ backgroundImage: `url(${backgroundmap})` }}
+        >
+            <div className="absolute top-0 left-0 w-full h-full bg-[#0B2545]/80 backdrop-blur-md"></div>
+        </div>
+    )
+}
+
+export default Background
