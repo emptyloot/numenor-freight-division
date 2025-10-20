787c328773a0bb746352b3bec5a0c2d0c4ced76f
emptyloot <daviddcmmoo@gmail.com>
2025-10-19 23:58:46 -0700

    update to tailwind

--- src/components/logo/Logo.js
+++ src/components/logo/Logo.js
@@ -1,11 +1,10 @@
 import logoImage from '../../assets/Flag_of_Numenor.webp'
-import './logo.css'
 function Logo() {
     return (
         <div>
-            <img className='logo-image' src={logoImage} alt="Numenor Frieght Division Logo" />
+            <img className='w-auto h-full blox max-h-[150px]' 
+            src={logoImage} alt="Numenor Frieght Division Logo" />
         </div>
     )
 }
-
 export default Logo;
