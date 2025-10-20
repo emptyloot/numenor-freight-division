1caf6732d8802bafbadbd879661d1e78cab75aae
emptyloot <daviddcmmoo@gmail.com>
2025-10-20 11:11:11 -0700

    update calculator

--- src/utils/Calculator.js
+++ src/utils/Calculator.js
@@ -1,9 +1,7 @@
 
 function Calculator(a,b) {
-    console.log('Calculator Called')
     const aSize = Number(a)
     const bSize = Number(b)
-    console.log(a)
     if (aSize <= 0 || bSize <= 0) {
         return 0
     }
