0ccd828d735322ae1446ee979e38a4827b5ecd6e
emptyloot <daviddcmmoo@gmail.com>
2025-10-20 11:12:01 -0700

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Homepage from './components/homepage/Homepage.js';
+import Background from './components/background/Background.js'
+import Header from './components/header/Header.js'
+import About from './components/aboutus/About.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+        <Background />
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ function Header() {
         <div className='mx-auto flex justify-between items-center'>
             <Logo/>
             <div className="hidden md:flex items-center space-x-4">
-                    <a href="#" className="border-2 border-[#FFC107] text-[#FFC107] px-4 py-2 rounded-full hover:bg-[#FFC107] hover:text-[#0B2545] transition-colors">
+                    <a href="/about" className="border-2 border-[#FFC107] text-[#FFC107] px-4 py-2 rounded-full hover:bg-[#FFC107] hover:text-[#0B2545] transition-colors">
                         About us
                     </a>
                     <a href="#" className="bg-[#FFC107] text-[#0B2545] font-bold px-6 py-2 rounded-full hover:opacity-90 transition-opacity">
