338d037558878ed9bce5a02930191e8fd16c7ba7
emptyloot <daviddcmmoo@gmail.com>
2025-10-21 14:47:22 -0700

    update return type in doc

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import Background from './components/background/Background.js';
 import Header from './components/header/Header.js';
 import About from './components/aboutus/About.js';
 import CreateShipment from './components/scheduleshipmentspage/CreateShipment.js';
-import React from 'react';
 
 /**
 @description The main application component that assembles the page layout and defines the routes.
-@returns {React.element} The rendered application with a persistent background and header.
+@returns {object} (JSX.element) The rendered application with a persistent background and header.
  */
 function App() {
   return (
