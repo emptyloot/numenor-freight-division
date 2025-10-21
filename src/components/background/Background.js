21b268dbde477565167ac431f39756147e499370
emptyloot <daviddcmmoo@gmail.com>
2025-10-21 13:56:23 -0700

    Fix spelling

--- src/components/background/Background.js
+++ src/components/background/Background.js
@@ -1,14 +1,14 @@
-import backgroundmap from '../../assets/bitcraftmapbackground.png';
+import backGroundMap from '../../assets/bitcraftmapbackground.png';
 
-function Background() {
+function BackGround() {
   return (
     <div
       className="fixed top-0 left-0 w-full h-full z-[-1] bg-cover bg-center"
-      style={{ backgroundImage: `url(${backgroundmap})` }}
+      style={{ backgroundImage: `url(${backGroundMap})` }}
     >
       <div className="absolute top-0 left-0 w-full h-full bg-[#0B2545]/80 backdrop-blur-md"></div>
     </div>
   );
 }
 
-export default Background;
+export default BackGround;
