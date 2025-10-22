a25863827f611879a4dbf397ab7d343f2666d6fd
emptyloot <daviddcmmoo@gmail.com>
2025-10-21 18:35:53 -0700

    fix: lint automatic fix

--- src/App.test.js
+++ src/App.test.js
@@ -10,44 +10,38 @@ import App from './App';
  * @description Renders the full App components and verifys text on screen.
  */
 test('renders the main homepage title', () => {
-  render(
-      <App />
-  );
+  render(<App />);
   // Check for the main H1 title on the homepage
   const headingElement = screen.getByText(/"By land or sea, the world turns on our trade"/i);
   expect(headingElement).toBeInTheDocument();
 });
-
 
 /**
  * @description Renders the full App and navigates to the about us page.
  */
 test('navigates to About page when About Us link is clicked', async () => {
   render(<App />);
-  
+
   // Find the "About us" link (case-insensitive)
   const aboutLink = screen.getByRole('link', { name: /about us/i });
-  
+
   // Simulate a user clicking the link
   await userEvent.click(aboutLink);
-  
+
   // Check if the heading of the About page is now visible
   const aboutHeading = screen.getByRole('heading', { name: /about númenor freight division/i });
   expect(aboutHeading).toBeInTheDocument();
 });
 
 test('starts at About page and navigates back to calculator', async () => {
-  render(
-    <App initialEntries={['/about']} />
-);
+  render(<App initialEntries={['/about']} />);
 
-//Find the Calculator link (case insensitive)
-const calculatorLink = screen.getByRole('link', {name: /calculator/i });
+  //Find the Calculator link (case insensitive)
+  const calculatorLink = screen.getByRole('link', { name: /calculator/i });
 
-await userEvent.click(calculatorLink);
+  await userEvent.click(calculatorLink);
 
-const calculatorHeading = screen.getByRole('heading',{name: /Instant Quote Calculator/i})
+  const calculatorHeading = screen.getByRole('heading', { name: /Instant Quote Calculator/i });
 
-expect(calculatorHeading).toBeInTheDocument();
-
+  expect(calculatorHeading).toBeInTheDocument();
 });
