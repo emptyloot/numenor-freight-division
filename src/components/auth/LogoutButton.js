5b1d1803e8b7013311d18f279ae62baee5ccb2a8
emptyloot <daviddcmmoo@gmail.com>
2025-10-23 17:39:15 -0700

    add logoutbutton to be discord-ish

---
+++ src/components/auth/LogoutButton.js
@@ -0,0 +1,8 @@
+/**
+@description Renders a button to log the user out.
+@returns {object} Render of the logout button
+ */
+const LogoutButton = () => {
+  return <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</button>;
+};
+export default LogoutButton;
