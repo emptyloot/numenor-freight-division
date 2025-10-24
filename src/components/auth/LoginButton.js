7bd1c6b99a2ca642e633b3ef2c7fc8c33e60f860
emptyloot <daviddcmmoo@gmail.com>
2025-10-23 22:19:40 -0700

    Button login/out to use context

--- src/components/auth/LoginButton.js
+++ src/components/auth/LoginButton.js
@@ -1,14 +1,21 @@
+import { useAuth } from '../../context/AuthContext';
 /**
-@description  Renders a button to initiate the user authentication flow.
+ @description  Renders a button to initiate the user authentication flow.
  When clicked, it triggers the sign-in process via a specified provider, such as Discord.
  It can be customized with properties to handle login events and disabled states.
-@returns {object} Render of the login button
+  @returns {object} Render of the login button
  */
 const LoginButton = () => {
+  const { setIsAuthenticated } = useAuth();
+
   return (
-    <button className="bg-[#5865F2] hover:bg-[#4752C4] text-white font-bold py-2 px-4 rounded-md flex items-center">
+    <button
+      onClick={() => setIsAuthenticated(true)}
+      className="bg-[#5865F2] hover:bg-[#4752C4] text-white font-bold py-2 px-4 rounded-md flex items-center"
+    >
       Login with Discord
     </button>
   );
 };
+
 export default LoginButton;
--- src/components/auth/LogoutButton.js
+++ src/components/auth/LogoutButton.js
@@ -1,8 +1,19 @@
+import { useAuth } from '../../context/AuthContext';
+
 /**
 @description Renders a button to log the user out.
 @returns {object} Render of the logout button
  */
 const LogoutButton = () => {
-  return <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</button>;
+  const { setIsAuthenticated } = useAuth();
+
+  return (
+    <button
+      onClick={() => setIsAuthenticated(false)}
+      className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+    >
+      Logout
+    </button>
+  );
 };
 export default LogoutButton;
