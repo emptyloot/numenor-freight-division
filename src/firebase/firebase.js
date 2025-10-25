5946fe9a5ad15d8c438f64c41d3afd9fbfc47e5a
emptyloot <daviddcmmoo@gmail.com>
2025-10-25 12:08:36 -0700

    Add emulator for app expansion

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { initializeApp } from 'firebase/app';
 import { getAuth, connectAuthEmulator } from 'firebase/auth';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDyOnvFQtAGTlJqp9Up4OkCKDDuA33dg-U',
@@ -16,7 +17,10 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+// Initialize Cloud Firestore and get a reference to the service
+export const firestore = getFirestore(app);
 
 if (window.location.hostname === 'localhost') {
   connectAuthEmulator(auth, 'http://localhost:9099');
+  connectFirestoreEmulator(firestore, 'localhost', 8080);
 }
