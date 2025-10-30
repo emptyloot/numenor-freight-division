9412d8e28a9e0152ce6fb30800889dca6382324b
emptyloot <daviddcmmoo@gmail.com>
2025-10-30 15:56:35 -0700

    refactor: Replace placeholder with final cost calculation logic in Calculator
    The `Calculator` function no longer returns a simple multiplication
    to test website design. It now contains the final logic for calculating
    the cost estimate based on port coordinates and defined constants.

--- src/utils/Calculator.js
+++ src/utils/Calculator.js
@@ -4,13 +4,54 @@
 @returns {number} result of the length a times length b to test website design
  */
 function Calculator(manifest) {
-  const aSize = Number(manifest.port[0].north) || 0;
-  const bSize = Number(manifest.port[1].north) || 0;
-  if (aSize <= 0 || bSize <= 0) {
+  //Set constants
+  const startLocation = manifest.port[0];
+  const endLocation = manifest.port[1];
+  //Exit early if still 0
+  if ((startLocation.north === 0 && startLocation.east === 0) || (endLocation.north === 0 && endLocation.east === 0)) {
     return 0;
   }
-  const result = aSize * bSize;
-  return result;
+  //more constants
+  const baseContractCost = 4500;
+  const shortHaulDistance = 2500;
+  const ShortHaulDiscountOffer = -2000;
+  const distanceErrorCorrectionTerm = 1.1;
+  const tpEnergyPerDistance = 0.004;
+  const pricePerFruit = 5000;
+  const tpEnergyPerFruit = 20;
+  const costPerDistance = 4;
+  const capitalLocation = { north: 4005, east: 6577 };
+
+  /**
+   * @description calculates the distance between two points using EuclideanDistance
+   * @param {object} startLocation object with north east cords for port of origin
+   * @param {object} endLocation object with north east cords for destination
+   * @returns {number} distance between two points
+   */
+  const getDistance = (startLocation, endLocation) => {
+    const distance = Math.sqrt(
+      Math.pow(startLocation.north - endLocation.north, 2) + Math.pow(startLocation.east - endLocation.east, 2)
+    );
+    return distance;
+  };
+  //Calculate the distance between two points.
+  const distance = getDistance(startLocation, endLocation);
+
+  const distanceToCapital = getDistance(startLocation, capitalLocation);
+
+  //Calculate Outbound TP  Cost
+  const outboundTpCost = Math.ceil(
+    (distanceErrorCorrectionTerm * distanceToCapital * tpEnergyPerDistance * pricePerFruit) / tpEnergyPerFruit
+  );
+
+  //Calculate Transport Distance Cost
+  const shortHaulDiscount = distance <= shortHaulDistance ? ShortHaulDiscountOffer : 0;
+  const transportDistanceCost = costPerDistance * distance + shortHaulDiscount;
+
+  //Calculate the cost estimate
+  const costEstimate = Math.ceil(baseContractCost + outboundTpCost + transportDistanceCost);
+
+  return costEstimate;
 }
 
 export default Calculator;
